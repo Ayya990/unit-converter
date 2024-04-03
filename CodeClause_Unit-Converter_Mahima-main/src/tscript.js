4bad433e9e18d7acf30ddcee0108a9a068ba9408
Toutaaaa <ayahajali02@gmail.com>
2024-04-03 02:10:06 +0100

    cc les babies

--- CodeClause_Unit-Converter_Mahima-main/src/tscript.js
+++ CodeClause_Unit-Converter_Mahima-main/src/tscript.js
@@ -20,49 +20,49 @@ function myResult(){
 
     if(inputTypeValue === "Celsius" && resultTypeValue==="Celsius"){
        
-        result.value = Number(input.value); // set answer to the result box
+        result.value = Number(input.value); 
     }
 
     else if(inputTypeValue === "Celsius" && resultTypeValue==="Kelvin"){
        
-        result.value = Number(input.value) + 273.15; // set answer to the result box
+        result.value = Number(input.value) + 273.15; 
     }
 
     else if(inputTypeValue === "Celsius" && resultTypeValue==="Fahrenheit"){
         
-        result.value = ((Number(input.value)*9)/5) + 32 ; // set answer to the result box    
+        result.value = ((Number(input.value)*9)/5) + 32 ;    
     }
 
 
     else if(inputTypeValue === "Kelvin" && resultTypeValue==="Celsius"){
        
-        result.value = Number(input.value) - 273.15; // set answer to the result box
+        result.value = Number(input.value) - 273.15; 
     }
 
     else if(inputTypeValue === "Kelvin" && resultTypeValue==="Kelvin"){
        
-        result.value = Number(input.value); // set answer to the result box
+        result.value = Number(input.value); 
     }
 
     else if(inputTypeValue === "Kelvin" && resultTypeValue==="Fahrenheit"){
         
-        result.value = ((Number(input.value)-273.15)*0.555556)+32 ; // set answer to the result box    
+        result.value = ((Number(input.value)-273.15)*0.555556)+32 ;    
     }
 
 
     else if(inputTypeValue === "Fahrenheit" && resultTypeValue==="Celsius"){
        
-        result.value = (Number(input.value)-32) * 0.555556; // set answer to the result box
+        result.value = (Number(input.value)-32) * 0.555556; 
     }
 
     else if(inputTypeValue === "Fahrenheit" && resultTypeValue==="Kelvin"){
        
-        result.value = ((Number(input.value) - 32) * 0.555556) + 273.15 ; // set answer to the result box
+        result.value = ((Number(input.value) - 32) * 0.555556) + 273.15 ; 
     }
 
     else if(inputTypeValue === "Fahrenheit" && resultTypeValue==="Fahrenheit"){
         
-        result.value = input.value ; // set answer to the result box    
+        result.value = input.value ;    
     }
 
 
